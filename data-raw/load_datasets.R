76ac4c848cd34377fce00fd46d7b9d93233afbc9
eblondel <emmanuel.blondel1@gmail.com>
2026-09-11 09:27:56 +0200

    ligthen RAW.ALL_AGG

--- data-raw/load_datasets.R
+++ data-raw/load_datasets.R
@@ -48,6 +48,7 @@ RAW.ALL = data.table::rbindlist(list(
 	RAW.OTHR
 ),use.names = TRUE, fill = TRUE)
 RAW.ALL_AGG = RAW.ALL[, .(FISH_COUNT = sum(FISH_COUNT)), keyby = setdiff(names(RAW.ALL), c("MONTH_START", "MONTH_END", "FISH_COUNT"))]
+RAW.ALL_AGG = RAW.ALL_AGG[, c("SPECIES_SCIENTIFIC", "SPECIES_FAMILY", "SPECIES_ORDER", "IS_IOTC_SPECIES", "IS_SPECIES_AGGREGATE", "IS_SSI") := NULL]
 usethis::use_data(RAW.ALL_AGG, overwrite = TRUE, compress = "gzip")
 
 LAST_UPDATE = Sys.Date()
